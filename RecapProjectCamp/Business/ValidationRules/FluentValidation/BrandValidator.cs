57f2fcb0fd0b87741951dc5cf5c3d05f3b778e87
gafar113 <falgamerss@gmail.com>
2021-02-21 15:31:14 +0300

    gitignore eklendi, bir adet unique methodu eklendi

--- RecapProjectCamp/Business/ValidationRules/FluentValidation/BrandValidator.cs
+++ RecapProjectCamp/Business/ValidationRules/FluentValidation/BrandValidator.cs
@@ -1,9 +1,10 @@
-﻿using Entities.Concrete;
+﻿using DataAccess.Concrete.Entity_Framework;
+using Entities.Concrete;
 using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.Linq;
 namespace Business.ValidationRules.FluentValidation
 {
     public class BrandValidator:AbstractValidator<Brand>
@@ -12,7 +13,16 @@ namespace Business.ValidationRules.FluentValidation
         {
             RuleFor(p => p.BrandName).NotEmpty();
             RuleFor(p => p.BrandName).MinimumLength(2);
-               
+            RuleFor(p => p.BrandName).Must(IsBrandUnique);
+        }
+
+        private bool IsBrandUnique(string arg)
+        {
+            CarBrandContext context = new CarBrandContext();
+            var result = context.Brands.Where(p => p.BrandName.ToLower() == arg.ToLower()).SingleOrDefault();
+            if (result == null)
+                return true;
+            return false;
         }
     }
 }
